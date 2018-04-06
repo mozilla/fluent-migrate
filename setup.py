605370a0fda653bb14e5ae8bc79106ac5056c7ec
Axel Hecht <axel@pike.org>
2018-04-06 12:35:58 +0200

    bug 1452046, fix up setup.py for fluent.migrate, r=stas

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ from setuptools import setup
 
 setup(name='fluent.migrate',
       version='0.6.4',
-      description='Localization library for expressive translations.',
+      description='Toolchain to migrate legacy translation to Fluent.',
       author='Mozilla',
       author_email='l10n-drivers@mozilla.org',
       license='APL 2',
-      url='https://github.com/projectfluent/python-fluent',
+      url='https://hg.mozilla.org/l10n/fluent-migration/',
       keywords=['fluent', 'localization', 'l10n'],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.5',
       ],
       packages=['fluent.migrate'],
-      tests_require=['six'],
+      install_requires=[
+          'compare-locales >=3.0, <4.0'
+      ],
       test_suite='tests.migrate'
 )
